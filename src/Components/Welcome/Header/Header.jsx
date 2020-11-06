a17501e36a6905370864ee782820f02d8e38e426
Oleg Stahniuk <oleh.stahnyuk@gmail.com>
2020-11-06 18:25:59 +0200

    xz sho tam

--- src/Components/Welcome/Header/Header.jsx
+++ src/Components/Welcome/Header/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import H from './Header.module.css'
 const Header = () => {
     return (
-        <a href="" className={H.header__href}>
+        <a href="_" className={H.header__href}>
             <img src="https://www.pngkit.com/png/full/55-558373_tinder-apple-tv-survey-methodology.png" alt="logo" className={H.header__img} />
             <div className={H.header__title}>Tinder for Animals</div>
         </a>
