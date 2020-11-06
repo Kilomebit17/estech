c72c38d839fbbe2f7c8315ce1850a0cd560227e9
Oleg Stahniuk <oleh.stahnyuk@gmail.com>
2020-11-06 16:25:26 +0200

    some change on header

--- src/Components/App/Profile/Header/Header.jsx
+++ src/Components/App/Profile/Header/Header.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import HeaderCss from './Header.module.css'
 
-const Header = (props) => {
+const Header = () => {
     return (
         <div className={HeaderCss.header}>
             <div className={HeaderCss.header__logo}>
                 <a href="">
                     <img className={HeaderCss.header__img} src="https://scontent.fiev9-1.fna.fbcdn.net/v/t1.0-9/96589892_852958135226289_9180315968964919296_n.jpg?_nc_cat=110&ccb=2&_nc_sid=09cbfe&_nc_ohc=rhEwhxnI6e8AX_v7o7G&_nc_ht=scontent.fiev9-1.fna&oh=fd164355b38746a8a73b3052d6f245f2&oe=5FC7FEFE" alt="logo" />
                 </a>
-                <a href="" className={HeaderCss.header__title}>Профиль</a>
+                <a href="" className={HeaderCss.header__title}>Мой профиль</a>
             </div>
         </div>
     )
